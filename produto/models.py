a9a53c5e7af56ecc7d28901345c818f3f5099455
PedroH-Teixeira <teixeira.phenrique@gmail.com>
2022-10-24 17:54:52 -0300

    mudança nas pastas

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,24 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+   # imagem = models.ImageField(
+    #    upload_to ='produto_imagens/%Y/%m/', blank=True, null=True
+    #)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocial = models.FloatField(default=0)
+    tipo = models.CharField(
+        default = 'V',
+        max_length = 1,
+        choices = (
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+    def __str__(self):
+        return self.nome

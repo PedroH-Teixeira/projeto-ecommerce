54216b923dc6608c10898c5a9d5c854d75387afb
PedroH-Teixeira <teixeira.phenrique@gmail.com>
2022-10-27 02:27:56 -0300

    Criado os views e urls

--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class Pagar(View):
+    pass
+
+class FecharPedido(View):
+    pass
+
+class Detalhe(View):
+    pass
